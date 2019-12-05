41992865cf78c51c8d919e9dab3750e0948e66c4
Bass Jansson <tech@bassjansson.com>
2019-12-05 12:11:16 +0100

    Adds initial hello world source with some basic libraries included.

---
+++ src/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+#include <portaudio.h>
+#include <portmidi.h>
+#include <sndfile.h>
+#include <fftw3.h>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+    cout << "\nHello World! :)\n" << endl;
+}
