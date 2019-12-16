9d82a8235ef754ecd52d0ea65d4a8425956905ff
Bass Jansson <tech@bassjansson.com>
2019-12-16 15:27:47 +0100

    Writes pre recording buffer to beginning of recording buffer, adds some fixes and adds a lot of comments.

--- src/Track.h
+++ src/Track.h
@@ -52,29 +52,53 @@ public:
 
     void startRecording()
     {
-        recStartFrame   = 0;
-        recLengthFrames = 0;
+        // Return if we are already recording
+        if (trackState == RECORDING)
+            return;
 
+        // Reset start frame and recording length
+        recStartFrame   = 0;
+        recLengthFrames = preRecLengthFrames;
+
+        // Clear track buffer
         for (frame_t i = 0; i < trackBufferSize; ++i)
             trackBuffer[i] = 0.0f;
 
+        // Start recording
         trackState = RECORDING;
+
+        // Write pre recording buffer to track buffer
+        for (frame_t i = 0; i < preRecBufferSize; ++i)
+            trackBuffer[i] = preRecBuffer[(preRecPointerFrame * TRACK_NUM_CHANNELS + i) % preRecBufferSize];
     }
 
     void stopRecording(bool shouldStartPlayback)
     {
-        stopPlayback();
+        // Return if we are not recording
+        if (trackState != RECORDING)
+            return;
 
+        // Stop recording
+        trackState = STOPPED;
+
+        // Save initial minimum recording length
         frame_t minRecLengthFrames = recLengthFrames;
 
+        // Remove pre recording length at end of recording length
+        recLengthFrames -= preRecLengthFrames;
+
+        // Check if this is the very first recording which will set the fraction denominator
         if (recLengthDenominator == 0)
         {
+            // Set the fraction denominator to the current recording length
             recLengthDenominator = recLengthFrames;
 
+            // Log it
             cout << "[Track " << trackIndex + 1 << "] ";
             cout << "Setting track and denominator recording length to ";
             cout << (float) recLengthDenominator / AUDIO_SAMPLE_RATE << " seconds." << endl;
         }
+        // Else, round the recording length to the nearest fraction
         else
         {
             double recLengthFraction = (double) recLengthFrames / recLengthDenominator;
@@ -86,6 +110,7 @@ public:
             double chosenFraction   = 1.0;
             int chosenFractionIndex = FRACTIONS_ARRAY_LENGTH / 2;
 
+            // Find the nearest fraction
             for (int i = 0; i < FRACTIONS_ARRAY_LENGTH; ++i)
             {
                 idealFraction = (double) FRACTIONS_ARRAY[i].x / FRACTIONS_ARRAY[i].y;
@@ -99,25 +124,34 @@ public:
                 }
             }
 
+            // Set recording length to the chosen fraction and clip it by the maximum
             recLengthFrames = (frame_t) (recLengthDenominator * chosenFraction + 0.5);
 
+            if (recLengthFrames > recLengthFramesMax)
+                recLengthFrames = recLengthFramesMax;
+
+            // Log it
             cout << "[Track " << trackIndex + 1 << "] ";
             cout << "Setting track recording length to ";
             cout << FRACTIONS_ARRAY[chosenFractionIndex].x << "/";
             cout << FRACTIONS_ARRAY[chosenFractionIndex].y << " of denominator." << endl;
         }
 
+        // In case recording length got smaller than the minimum, set the minimum to it
         if (recLengthFrames < minRecLengthFrames)
             minRecLengthFrames = recLengthFrames;
 
+        // Get fade length within the bounderies of the minimum recording length
         frame_t fadeLengthFrames = TRACK_FADE_LENGTH * AUDIO_SAMPLE_RATE / 1000;
 
         if (fadeLengthFrames * 2 > minRecLengthFrames)
             fadeLengthFrames = minRecLengthFrames / 2;
 
+        // Fade in and fade out the track buffer
+        float f;
         for (frame_t i = 0; i < fadeLengthFrames; ++i)
         {
-            float f = (float) i / (fadeLengthFrames - 1);
+            f = (float) i / (fadeLengthFrames - 1);
 
             // Fade in
             trackBuffer[i * TRACK_NUM_CHANNELS + LEFT]  *= f;
@@ -128,18 +162,30 @@ public:
             trackBuffer[(minRecLengthFrames - 1 - i) * TRACK_NUM_CHANNELS + RIGHT] *= f;
         }
 
+        // Start playback if requested
         if (shouldStartPlayback)
             startPlayback();
     } // stopRecording
 
     void startPlayback()
     {
+        // Return if we are already playing
+        if (trackState == PLAYING)
+            return;
+
+        // Start playback
         shouldFadeIn = true;
         trackState   = PLAYING;
     }
 
     void stopPlayback()
     {
+        // Return if we are not playing
+        if (trackState != PLAYING)
+            return;
+
+        // Stop playback
+        // TODO: add fade out
         trackState = STOPPED;
     }
 
@@ -198,13 +244,17 @@ public:
         int           numOutputChannels,
         frame_t       currentFrame)
     {
+        // Check if we are recording
         if (trackState == RECORDING)
         {
-            if (recLengthFrames == 0)
-                recStartFrame = currentFrame - (TRACK_PREREC_LENGTH * AUDIO_SAMPLE_RATE / 1000);
+            // In case the start frame is not set yet, set it to the current frame
+            if (recStartFrame == 0)
+                recStartFrame = currentFrame - preRecLengthFrames;
 
-            if (recLengthFrames + framesPerBuffer < recLengthFramesMax)
+            // Check if we have space left in the track buffer for recording
+            if (recLengthFrames + framesPerBuffer <= recLengthFramesMax)
             {
+                // Write input to track buffer
                 for (frame_t i = 0; i < framesPerBuffer; ++i)
                 {
                     trackBuffer[recLengthFrames * TRACK_NUM_CHANNELS + LEFT] =
@@ -215,13 +265,25 @@ public:
                     recLengthFrames++;
                 }
             }
+            // Else, clear and stop the recording
             else
             {
+                // Reset start frame and recording length
+                recStartFrame   = 0;
+                recLengthFrames = 0;
+
+                // Clear track buffer
+                for (frame_t i = 0; i < trackBufferSize; ++i)
+                    trackBuffer[i] = 0.0f;
+
+                // Stop recording
                 trackState = STOPPED;
             }
         }
-        else // trackState != RECORDING
+        // Else, we are in playback
+        else
         {
+            // TODO: Should we do this also during record?
             // Write input to pre recording buffer
             for (frame_t i = 0; i < framesPerBuffer; ++i)
             {
@@ -233,39 +295,42 @@ public:
                 preRecPointerFrame = (preRecPointerFrame + 1) % preRecLengthFrames;
             }
 
-            if (trackState == PLAYING)
+            // Check if we are playing and have any frames to play
+            if (trackState == PLAYING && recLengthFrames > 0)
             {
-                if (recLengthFrames > 0)
+                // Check if we should fade in
+                if (shouldFadeIn)
                 {
+                    // We shouldn't fade in a second time
+                    shouldFadeIn = false;
+
+                    // Write track buffer to output with fade in
                     frame_t p;
-
-                    if (shouldFadeIn)
+                    float f;
+                    for (frame_t i = 0; i < framesPerBuffer; ++i)
                     {
-                        shouldFadeIn = false;
-                        float f;
+                        p = (currentFrame + i - recStartFrame) % recLengthFrames;
+                        f = (float) i / (framesPerBuffer - 1);
 
-                        for (frame_t i = 0; i < framesPerBuffer; ++i)
-                        {
-                            p = (currentFrame + i - recStartFrame) % recLengthFrames;
-                            f = (float) i / (framesPerBuffer - 1);
-
-                            outputBuffer[i * numOutputChannels + LEFT] +=
-                              trackBuffer[p * TRACK_NUM_CHANNELS + LEFT] * f;
-                            outputBuffer[i * numOutputChannels + RIGHT] +=
-                              trackBuffer[p * TRACK_NUM_CHANNELS + RIGHT] * f;
-                        }
+                        outputBuffer[i * numOutputChannels + LEFT] +=
+                          trackBuffer[p * TRACK_NUM_CHANNELS + LEFT] * f;
+                        outputBuffer[i * numOutputChannels + RIGHT] +=
+                          trackBuffer[p * TRACK_NUM_CHANNELS + RIGHT] * f;
                     }
-                    else
+                }
+                // Else, just play
+                else
+                {
+                    // Write track buffer to output
+                    frame_t p;
+                    for (frame_t i = 0; i < framesPerBuffer; ++i)
                     {
-                        for (frame_t i = 0; i < framesPerBuffer; ++i)
-                        {
-                            p = (currentFrame + i - recStartFrame) % recLengthFrames;
+                        p = (currentFrame + i - recStartFrame) % recLengthFrames;
 
-                            outputBuffer[i * numOutputChannels + LEFT] +=
-                              trackBuffer[p * TRACK_NUM_CHANNELS + LEFT];
-                            outputBuffer[i * numOutputChannels + RIGHT] +=
-                              trackBuffer[p * TRACK_NUM_CHANNELS + RIGHT];
-                        }
+                        outputBuffer[i * numOutputChannels + LEFT] +=
+                          trackBuffer[p * TRACK_NUM_CHANNELS + LEFT];
+                        outputBuffer[i * numOutputChannels + RIGHT] +=
+                          trackBuffer[p * TRACK_NUM_CHANNELS + RIGHT];
                     }
                 }
             }
